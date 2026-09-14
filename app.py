a2161bc86acfcc20978edde836ee19b0ef8ad5bf
Isai Perez Tecuautzin <IsaiPeTe@gmail.com>
2026-09-13 23:16:50 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 st.title("Tracker de Finanzas Personales")
 st.write("Lleva el control de tus gastos e in gresos de manera simple y visual")
-st.markdown("Version 1.0.0")
+st.caption("Version 1.0.0")
+
+categorias=["Alimentacion","Transporte","Entretenimiento","Salud","Educacion","Otros"]
 
 descripcion=st.text_input("Descripcion del gasto o ingreso",placeholder="Escribe la descripcion de la operacion")
 monto=st.number_input("Monto",step=1.0,min_value=0.0,format="%0.2f")
+fecha=st.date_input("Fecha")
+categoria=st.selectbox("Categoria",categorias)

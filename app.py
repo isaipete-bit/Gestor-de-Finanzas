da1a4051a67e91076bff058bbd3bd7a4b68e7b6b
Isai Perez Tecuautzin <IsaiPeTe@gmail.com>
2026-09-13 23:13:01 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.write("Lleva el control de tus gastos e in gresos de manera simple y visual")
 st.markdown("Version 1.0.0")
 
 descripcion=st.text_input("Descripcion del gasto o ingreso",placeholder="Escribe la descripcion de la operacion")
-st.write("Escribiste: ", descripcion)
+monto=st.number_input("Monto",step=1.0,min_value=0.0,format="%0.2f")
